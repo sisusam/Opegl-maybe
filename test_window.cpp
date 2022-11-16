6c80c7660319d175e70f01b3ccb2a4489f36c2c8
sisusam <109479539+sisusam@users.noreply.github.com>
2022-11-16 14:33:14 +0000

    Here, have code

---
+++ test_window.cpp
@@ -0,0 +1,10 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+// Include GLEW. Always include it before gl.h and glfw3.h, since it's a bit magic.
+#include <GL/glew.h>
+// Include GLFW
+#include <GLFW/glfw3.h>
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
